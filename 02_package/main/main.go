bec8c8aa0c60fc9b4e0900c97f2e07807f0b5183
Jeremy <bossco28@gmail.com>
2016-01-14 07:57:54 -0500

    Init msg

---
+++ 02_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/GolangTraining/GolangTrainingUdemy/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("ti od s'tel !oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
